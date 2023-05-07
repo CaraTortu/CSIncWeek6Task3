cdf16aab254def5c1aa49b564e0cfbd9facb2bf8
CaraTortu <javierdj@jadk.com>
2023-05-07 21:08:09 +0100

    update styling to respect navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
+    <main className="w-full flex flex-col items-center justify-center">
       <h1>Main</h1>
     </main>
   )
--- src/components/PageSchema.tsx
+++ src/components/PageSchema.tsx
@@ -12,7 +12,7 @@ const BackgroundNoSSR = dynamic(
 const PageSchema = (props: React.PropsWithChildren) => {
 
     return (
-        <div className="h-screen w-full flex flex-col items-center justify-center select-none">
+        <div className="min-h-screen w-full flex flex-col items-center justify-center select-none">
             <BackgroundNoSSR />
             <Navbar />
             <div className="pt-20 h-full w-full flex flex-col items-center justify-center text-white z-0">
